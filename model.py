2adee7b702159368dac7d741ef40c1fd0a3ff5ed
Ivan D. Grigoryev <igrigoryev@bsvt-nt.by>
2020-06-15 17:45:55 +0300

    Model improvements

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 """This file contains tools for working with network model"""
 
-from keras.optimizers import Adam
+from typing import List
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D
@@ -10,39 +10,38 @@ from keras.layers.core import Dense
 from keras.layers.core import Dropout
 from keras.layers import Flatten
 from keras.layers import Input
-from typing import Tuple
 
 
 def create_model(
-    width: int,
-    height: int,
-    depth: int = 1,
-    filters: Tuple[int, ...] = (16, 32, 64)
+        width: int,
+        height: int,
+        depth: int = 1,
+        filters: List[int] = [16, 32, 64]
         ) -> Model:
-    """Create cnn model architecture."""
+    """Create cnn model architecture.
+    """
 
-    input_shape = (width, height, depth)
+    input_shape = (height, width, depth)
     chan_dim = -1
     inputs = Input(shape=input_shape)
     for index, size in enumerate(filters):
         if index == 0:
             x = inputs
         # CONV -> RELU -> BN -> POOL
-        x = Conv2D(size, (3, 3), padding='same')(x)
-        x = Activation('relu')(x)
+        x = Conv2D(size, (3, 3),
+                   padding='same',
+                   kernel_initializer='he_normal')(x)
         x = BatchNormalization(axis=chan_dim)(x)
+        x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(2, 2))(x)
     x = Flatten()(x)
     # FC -> RELU -> BN -> DROPOUT
-    x = Dense(16)(x)
-    x = Activation('relu')(x)
+    x = Dense(512, kernel_initializer='he_normal')(x)
     x = BatchNormalization(axis=chan_dim)(x)
+    x = Activation('relu')(x)
     x = Dropout(0.5)(x)
 
-    x = Dense(4)(x)
-    x = Activation('relu')(x)
-
-    x = Dense(1)(x)
+    x = Dense(1, kernel_initializer='he_normal')(x)
     x = Activation('linear')(x)
 
     return Model(inputs, x)
